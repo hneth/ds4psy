0f19ee03f5f7668c9fb1e573fc50fa4306e7eddf
hneth <hansneth@gmail.com>
2018-03-10 18:10:04 +0100

    add stuff

--- R/05_data_transformation.R
+++ R/05_data_transformation.R
@@ -1,11 +1,14 @@
 ## r4ds: Chapter 5: Data transformation 
 ## Code for http://r4ds.had.co.nz/5_data_transformation.html 
 ## hn spds uni.kn
-## 2018 03 09 ------
+## 2018 03 10 ------
+
 
 ## Quotes: ------
 
 ## ...
+
+
 
 ## 5.1 Introduction ------
 
@@ -23,7 +26,6 @@ library(tidyverse)
 # dplyr overwrites some functions in base R. 
 # If you want to use the base version of these functions after loading dplyr, 
 # you’ll need to use their full names: stats::filter() and stats::lag().
-
 
 ?flights
 
@@ -47,16 +49,16 @@ table(flights$dest)
 # - date stands for dates. 
 
 
-# 5.1.3 dplyr basics ------
+## 5.1.3 dplyr basics ------
 
 # In this chapter you are going to learn the five key dplyr functions that allow you 
 # to solve the vast majority of your data manipulation challenges:
   
-# - Pick observations by their values (filter()).
-# - Reorder the rows (arrange()).
-# - Pick variables by their names (select()).
-# - Create new variables with functions of existing variables (mutate()).
-# - Collapse many values down to a single summary (summarise()).
+# 1. Pick observations by their values (filter()).
+# 2. Reorder the rows (arrange()).
+# 3. Pick variables by their names (select()).
+# 4. Create new variables with functions of existing variables (mutate()).
+# 5. Collapse many values down to a single summary (summarise()).
 
 # These can all be used in conjunction with group_by() which changes the 
 # scope of each function from operating on the entire dataset to operating 
@@ -64,13 +66,16 @@ table(flights$dest)
 # These six functions provide the verbs for a language of data manipulation:
 
 # All verbs work similarly:
+# 
 # - The first argument is a data frame.
 # - The subsequent arguments describe what to do with the data frame, 
 #   using the variable names (without quotes).
 # - The result is a new data frame.
 
 
-# 5.2 Filter rows with filter() ------
+
+
+## 5.2 Filter rows with filter() ------
 
 # filter() allows you to subset observations based on their values. 
 # The first argument is the name of the data frame. 
@@ -242,7 +247,195 @@ FALSE & NA
 
 NA * 0
 
+
+
+## 5.3 Arrange rows with arrange() ------
+
+## arrange() works similarly to filter() except that 
+## instead of selecting rows, it changes their order. 
+## It takes a data frame and a set of column names 
+## (or more complicated expressions) to order by. 
+
+## If you provide more than one column name, 
+## each additional column will be used to break ties 
+## in the values of preceding columns:
+  
+arrange(flights, year, month, day)
+
+## Use desc() to re-order by a column in descending order:
+  
+arrange(flights, desc(arr_delay))
+
+at <- arrange(flights, desc(air_time)) 
+at$air_time
+
+## Missing values are always sorted to the end:
+
+df <- tibble(x = c(5, 2, NA))
+
+arrange(df, x)
+arrange(df, desc(x))
+
+
+## 5.3.1 Exercises ------
+
+# 1. How could you use arrange() to sort all missing values to the start? (Hint: use is.na()).
+
+flights
+arrange(flights, !is.na(dep_time))
+
+# 2. Sort flights to find the most delayed flights. Find the flights that left earliest.
+
+(most_delay <- arrange(flights, desc(dep_delay)))
+(earliest <- arrange(flights, dep_time))
+
+# 3. Sort flights to find the fastest flights:
+# a) fastest = shortest:
+(fastest <- arrange(flights, air_time))
+
+# b) fastest = min arr_delay:
+(faster_than_schedule <- arrange(flights, arr_delay))
+
+## [test.quest]: From where to where did the 3 fastest flights go to?
+fastest # from EWR to BDL
+filter(airports, faa == "EWR" | faa == "BDL")
+
+# 4. Which flights travelled the longest? Which travelled the shortest?
+
+# time: 
+(longest <-  arrange(flights, desc(air_time)))
+(shortest <-  arrange(flights, air_time))
+
+# distance:
+(farthest <- arrange(flights, desc(distance)))
+
+filter(airports, faa == "JFK" | faa == "HNL")
+
+## [test.quest]: 
+## - What is the distance of the farthest flight (in terms of miles)?
+## - Is the farthest flight (in terms of miles) also the longest (in terms of time)?
+## - How many instances of the farthest flight are in the data? 
+## - What is their average duration?
+## - Which carrier(s) conducted these flights?
+## - Plot the relationship between both (distance and time).
+
+flights
+(farthest <- filter(flights, origin == "JFK" & dest == "HNL")) # => 342
+mean(farthest$air_time)
+table(farthest$carrier)
+
+?airlines
+filter(airlines, carrier == "HA")
+
+
+
+## 5.4 Select columns with select() ------
+
+# It’s not uncommon to get datasets with hundreds or even thousands of
+# variables. In this case, the first challenge is often narrowing in on the
+# variables you’re actually interested in.
+
+# select() allows you to rapidly zoom in on a useful subset using operations
+# based on the names of the variables.
+
+# select() is not terribly useful with the flights data because we only have 
+# 19 variables, but you can still get the general idea:
+
+select(flights, year, month, day)  # select columns by name
+select(flights, year:day)          # select all columns between year and day (inclusive)
+select(flights, -(year:day))       # select all columns except those from year to day (inclusive)
+
+# There are a number of helper functions you can use within select():
+#   
+# - starts_with("abc"): matches names that begin with “abc”.
+
+select(flights, starts_with("dep"))
+
+# - ends_with("xyz"):   matches names that end with “xyz”.
+
+select(flights, ends_with("time"))
+
+# - contains("ijk"):    matches names that contain “ijk”.
+
+select(flights, contains("arr"))
+
+# - matches("(.)\\1"):  selects variables that match a regular expression. 
+#                       This one matches any variables that contain repeated characters. 
+#                       You’ll learn more about regular expressions in strings.
+
+select(flights, matches("(.)\\1"))
+
+# - num_range("x", 1:3) matches x1, x2 and x3.
+# 
+# See ?select for more details.
+
+?select
+
+# See "Scoped selection and renaming"
+
+## select vs. rename:
+
+# select() can be used to rename variables, but it’s rarely useful 
+# because it drops all of the variables not explicitly mentioned. 
+# Instead, use rename(), which is a variant of select() that 
+# keeps all the variables that aren’t explicitly mentioned:
+
+?rename
+
+# flights$tailnum
+rename(flights, tail_num = "tailnum")
+
+# head(iris)
+# rename(iris, petal_length = Petal.Length)
+
+
+## everything:
+
+# Another option is to use select() in conjunction with the everything() helper.
+# This is useful if you have a handful of variables you’d like to move to the
+# start of the data frame: 
+
+select(flights, time_hour, air_time, everything())
+
+
+## 5.4.1 Exercises ------
+
+flights
+
+# 1. Brainstorm as many ways as possible to select dep_time, dep_delay, arr_time, and arr_delay from flights.
+
+select(flights, dep_time, dep_delay, arr_time, arr_delay)
+select(flights, dep_time, dep_delay:arr_time, arr_delay)
+select(flights, contains("time"), contains("delay"), -contains("sched"), -contains("air"), -contains("hour"))
+
+
+# 2. What happens if you include the name of a variable multiple times in a select() call?
+
+select(flights, dep_time, arr_time, dep_time, arr_time)  # each unique variable is only included once:
+
+
+# 3. What does the one_of() function do? Why might it be helpful in conjunction with this vector?
+   
+vars <- c("year", "month", "day", "dep_delay", "arr_delay", "something_else")
+
+?one_of # allows providing a vector of variable names to a select call:
+
+select(flights, one_of(vars))
+
+
+# 4. Does the result of running the following code surprise you? 
+# How do the select helpers deal with case by default? 
+# How can you change that default?
+  
+select(flights, contains("TIME"))  # yes, this is surprising, as R typically is case sensitive.  But here: ignore.case = TRUE by default.
+select(flights, contains("TIME", ignore.case = FALSE))
+
+
+
+
+
 ## +++ here now +++ ------ 
+
 
 
 ## Appendix: Additional resources on dplyr: ------
