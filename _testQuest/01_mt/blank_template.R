706fae0fce78ed7f5d4187b997b4f06b819e8449
hneth <hansneth@gmail.com>
2018-10-28 09:17:15 +0100

    move and copy folders

---
+++ _testQuest/01_mt/blank_template.R
@@ -0,0 +1,27 @@
+## Mid-term exam  | Data science for psychologists (Summer 2018)
+## Name: ... | Student ID: ...
+## 2018 06 04
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+## Preparations: ----- 
+
+library(tidyverse)
+
+
+## Task 1: ----- 
+
+## (a) Saving and inspecting data:
+sp <- as_tibble(sleep)
+dim(sp)
+
+## Answer: The sleep data contains 20 rows and 3 columns. 
+
+## (b): ... 
+## ...
+
+
+## Task X: ----- 
+## (a) ...
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+## End of file. ----- 
