8d89001c1b93a31099e55f1e6243e134a9393199
hneth <hansneth@gmail.com>
2019-04-15 18:37:01 +0200

    create file

---
+++ R/01_R_basics.R
@@ -0,0 +1,19 @@
+## R basics | ds4psy
+## 2019 04 15
+## ----------------------------
+
+## Preparations: ----------
+
+library(tidyverse)
+
+## Topic: ----------
+
+2 > 1
+
+
+
+
+
+# ...
+
+## End of file (eof). ----------  
