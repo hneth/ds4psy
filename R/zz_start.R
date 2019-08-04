27b3420e38893df459bd5395e727853dd2b2b933
hneth <hansneth@gmail.com>
2019-08-04 08:39:03 +0200

    add zz_start.R

---
+++ R/zz_start.R
@@ -0,0 +1,65 @@
+## zz_start.R | ds4psy
+## hn | uni.kn | 2019 08 04
+## ---------------------------
+
+## Open package guide: -----------------------
+
+#' Opens the unikn package guides
+#'
+#' @import utils
+#'
+#' @export
+
+ds4psy.guide <- function() {
+
+  # utils::vignette(topic = "User Guide", package = "ds4psy")
+  utils::browseVignettes(package = "ds4psy")
+
+}
+
+## Initialize package: ------
+
+.onAttach <- function(libname, pkgname) {
+
+  ## Welcome message: ------
+
+  pkg_version <- utils::packageVersion("ds4psy", lib.loc = NULL)
+
+  # welcome_message <- paste0("Welcome to ds4psy!")
+  welcome_message <- paste0("Welcome to ds4psy (v", pkg_version, ")!")
+  
+  packageStartupMessage(welcome_message)
+
+  ## User guidance: ------
+
+  ## Roll dice: ------
+  dice <- sample(1:6, 1)
+
+  if (dice == -77) {
+    pkg_version <- utils::packageVersion("ds4psy", lib.loc = NULL)
+    pkg_message <- paste0("Running ds4psy (v", pkg_version, ")...")
+
+    packageStartupMessage(" ")
+    packageStartupMessage(pkg_message)
+    packageStartupMessage(" ")
+  }
+
+  if (dice == -99) {
+    packageStartupMessage(" ")
+    packageStartupMessage("citation('ds4psy') provides citation info.")
+    packageStartupMessage(" ")
+  }
+
+  ## all cases:
+  # packageStartupMessage("ds4psy.guide() opens user guides.")
+
+}
+
+
+## Done: ------
+## - ...
+
+## ToDo: ------
+## - ...
+
+## eof. ----------
