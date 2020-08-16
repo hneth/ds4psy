f5a251ec757a82727505e2a4661f785f85fe7fb5
hneth <hansneth@gmail.com>
2020-08-16 11:43:49 +0200

    add is_equal() as a wrapper for num_equal() vs. ==, depending on data type of inputs

--- R/util_fun.R
+++ R/util_fun.R
@@ -1,5 +1,5 @@
 ## util_fun.R | ds4psy
-## hn | uni.kn | 2020 08 08
+## hn | uni.kn | 2020 08 16
 ## ---------------------------
 
 ## Utility functions. 
@@ -502,17 +502,17 @@ is_wholenumber <- function(x, tol = .Machine$double.eps^0.5) {
 # base::all.equal() and 
 # dplyr::near for similar functions.
 
-#' Test two numeric vectors for (near) equality. 
+#' Test two numeric vectors for pairwise (near) equality. 
 #'
 #' \code{num_equal} tests if two numeric vectors \code{x} and \code{y} are pairwise equal 
 #' (within some tolerance value `tol`). 
 #' 
 #' \code{num_equal} is a safer way to verify the (near) equality of numeric vectors than \code{==},  
-#' as numbers may contain floating point effects. 
+#' as numbers may exhibit floating point effects. 
 #' 
-#' @param x Number(s) to test (required, assumes a numeric vector).
+#' @param x 1st numeric vector to compare (required, assumes a numeric vector).
 #'
-#' @param y Number(s) to test (required, assumes a numeric vector).
+#' @param y 2nd numeric vector to compare (required, assumes a numeric vector).
 #'
 #' @param tol Numeric tolerance value.  
 #' Default: \code{tol = .Machine$double.eps^0.5} 
@@ -538,6 +538,7 @@ is_wholenumber <- function(x, tol = .Machine$double.eps^0.5) {
 #' @family utility functions
 #'
 #' @seealso 
+#' \code{\link{is_equal}} function for generic vectors;
 #' \code{\link{all.equal}} function of the R \strong{base} package;
 #' \code{near} function of the \strong{dplyr} package. 
 #'
@@ -545,8 +546,22 @@ is_wholenumber <- function(x, tol = .Machine$double.eps^0.5) {
 
 num_equal <- function(x, y, tol = .Machine$double.eps^0.5){
   
-  abs(x - y) < tol 
-
+  out <- NA  # initialize
+  
+  if (!is.numeric(x)) {
+    message("num_equal: x must be numeric.")
+    return(out)
+  }
+  
+  if (!is.numeric(y)) {
+    message("num_equal: y must be numeric.")
+    return(out)
+  }
+  
+  out <- abs(x - y) < tol 
+  
+  return(out)
+  
 } # num_equal end. 
 
 # ## Check:
@@ -565,7 +580,93 @@ num_equal <- function(x, y, tol = .Machine$double.eps^0.5){
 # unique(v)
 # .1 == v
 # num_equal(.1, v)
+#
+# # Dates:
+# num_equal(unclass(as.Date("2020-08-16")), unclass(Sys.Date()))
+# 
+# # non-numeric inputs:
+# num_equal(1:3 > 1, 1:3)
+# num_equal(1:3, LETTERS[1:3])
+# num_equal(as.Date("2020-08-16"), unclass(Sys.Date()))
 
+
+# is_equal: A wrapper around "==" and num_equal() ------ 
+
+#' Test two vectors for pairwise (near) equality. 
+#'
+#' \code{is_equal} tests if two vectors \code{x} and \code{y} are pairwise equal. 
+#' 
+#' If both \code{x} and \code{y} are numeric, 
+#' \code{is_equal} calls \code{num_equal(x, y, ...)} 
+#' (allowing for some tolerance threshold \code{tol}).  
+#' 
+#' Otherwise, \code{x} and \code{y} are compared by \code{x == y}. 
+#' 
+#' \code{is_equal} is a safer way to verify the (near) equality of numeric vectors than \code{==},  
+#' as numbers may exhibit floating point effects. 
+#' 
+#' @param x 1st vector to compare (required).
+#'
+#' @param y 2nd vector to compare (required).
+#'
+#' @param ... Other parameters (passed to \code{num_equal()}). 
+#'
+#' @examples
+#' 
+#' # numeric data: 
+#' is_equal(2, sqrt(2)^2)
+#' is_equal(2, sqrt(2)^2, tol = 0)
+#' is_equal(c(2, 3), c(sqrt(2)^2, sqrt(3)^2, 4/2, 9/3))
+#' 
+#' # other data types:
+#' is_equal((1:3 > 1), (1:3 > 2))                         # logical
+#' is_equal(c("A", "B", "c"), toupper(c("a", "b", "c")))  # character
+#' is_equal(as.Date("2020-08-16"), Sys.Date())            # dates
+#' 
+#' # as factors:
+#' is_equal((1:3 > 1), as.factor((1:3 > 2)))  
+#' is_equal(c(1, 2, 3), as.factor(c(1, 2, 3)))
+#' is_equal(c("A", "B", "C"), as.factor(c("A", "B", "C"))) 
+#'  
+#' @family utility functions
+#'
+#' @seealso 
+#' \code{\link{num_equal}} function for comparing numeric vectors;
+#' \code{\link{all.equal}} function of the R \strong{base} package;
+#' \code{near} function of the \strong{dplyr} package. 
+#'
+#' @export 
+
+is_equal <- function(x, y, ...){
+  
+  if (is.numeric(x) & is.numeric(y)){
+    
+    # message("is_equal: Passing numeric inputs to num_equal().")
+    num_equal(x, y, ...)
+    
+  } else { # all other data types: 
+    
+    x == y
+    
+  }
+  
+} # is_equal end. 
+
+## Check:
+# # numeric data: 
+# is_equal(2, sqrt(2)^2)
+# is_equal(2, sqrt(2)^2, tol = 0)
+# is_equal(c(2, 3), c(sqrt(2)^2, sqrt(3)^2, 4/2, 9/3))
+# 
+# # other data types:
+# is_equal((1:4 > 2), (1:4 > 3))  # logical/Boolean
+# is_equal(c("A", "B", "c"), toupper(c("a", "b", "c")))  # character
+# is_equal(as.Date("2020-08-16"), Sys.Date())
+#
+# # as factors:
+# is_equal((1:4 > 2), as.factor((1:4 > 3)))  
+# is_equal(c(1, 2, 3), as.factor(c(1, 2, 3)))
+# is_equal(c("A", "B", "C"), as.factor(c("A", "B", "C")))
 
 
 
