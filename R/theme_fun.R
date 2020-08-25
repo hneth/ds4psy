3829e02f38a759c431061d0d991f3a5caee686d4
hneth <hansneth@gmail.com>
2020-08-25 12:03:31 +0200

    revise theme_ds4psy to add margins and provide control over background and text colors

--- R/theme_fun.R
+++ R/theme_fun.R
@@ -1,5 +1,5 @@
 ## theme_fun.R | ds4psy
-## hn | uni.kn | 2020 05 06
+## hn | uni.kn | 2020 08 25
 ## ---------------------------
 
 ## Functions for ggplot2 themes. 
@@ -17,10 +17,8 @@
 #' opinionated (e.g., in using mostly grey scales to 
 #' allow emphasizing data points with color accents). 
 #' 
-#' @param col_title Color of title (text) elements (optional, numeric).  
-#' Default: \code{col_title = "black"}.  
-#' Consider using \code{col_title = unikn::pal_seeblau[[4]]} 
-#' in combination with black or grey data points. 
+#' Whereas the colors of text elements and the plot background can be specified, 
+#' the panel background is assumed to be white. 
 #' 
 #' @param base_size Base font size (optional, numeric). 
 #' Default: \code{base_size = 11}. 
@@ -33,6 +31,21 @@
 #' 
 #' @param base_rect_size Base rectangle size (optional, numeric). 
 #' Default: \code{base_rect_size = base_size/20}. 
+#' 
+#' @param col_title Color of plot title (text) elements (optional).  
+#' Default: \code{col_title = grey(.0, 1)} (i.e., "black").  
+#' 
+#' @param col_txt_1 Color of text elements of headings and axis labels (optional).  
+#' Default: \code{col_title = grey(.1, 1)}. 
+#' 
+#' @param col_txt_2 Color of text elements of legend and axis text/ticks (optional).  
+#' Default: \code{col_title = grey(.2, 1)}.  
+#' 
+#' @param col_txt_3 Color of text elements of facet strips (optional).  
+#' Default: \code{col_title = grey(.1, 1)}. 
+#' 
+#' @param col_bg Color of plot background (optional).  
+#' Default: \code{col_bg = "transparent"}.  
 #' 
 #' @examples
 #' 
@@ -59,83 +72,56 @@
 #' 
 #' @export
 
-theme_ds4psy <- function(col_title = "black", # OR: unikn::pal_seeblau[[4]], 
-                         base_size = 11, 
+theme_ds4psy <- function(base_size = 11, 
                          base_family = "", 
                          base_line_size = base_size/20, 
-                         base_rect_size = base_size/20
+                         base_rect_size = base_size/20,
+                         col_title = grey(.0, 1),  # "black" OR unikn::pal_seeblau[[4]], 
+                         col_txt_1 = grey(.1, 1),  # (used for headings and axis labels)
+                         col_txt_2 = grey(.2, 1),  # (used for legend and axis text/ticks)
+                         col_txt_3 = grey(.1, 1),  # (used for strip labels of facets)
+                         col_bg = "transparent"    # grey(1, 1) "white"
 ) {ggplot2::theme_bw(base_size = base_size, 
                      base_family = base_family, 
                      base_line_size = base_line_size, 
                      base_rect_size = base_rect_size) %+replace% 
     ggplot2::theme(#
       # titles: 
-      title = ggplot2::element_text(color = col_title, face = "bold"), 
-      plot.subtitle = ggplot2::element_text(color = grey(.10, 1), face = "plain", hjust = 0),  
-      plot.caption = ggplot2::element_text(color =  grey(.20, 1), face = "plain", size = ggplot2::rel(.80), hjust = 1), 
+      title = ggplot2::element_text(color = col_title, face = "bold", 
+                                    margin = ggplot2::margin(t = 10, r = 5, b = 8, l = 5, unit = "pt")), 
+      plot.subtitle = ggplot2::element_text(color = col_txt_1, face = "plain", hjust = 0,
+                                            margin = ggplot2::margin(t = 2, r = 5, b = 8, l = 5, unit = "pt")),  
+      plot.caption  = ggplot2::element_text(color = col_txt_2, face = "plain", size = ggplot2::rel(.80), hjust = 1), 
       # axes:
       axis.line =  ggplot2::element_line(color = "black", size = ggplot2::rel(1)), 
-      axis.ticks = ggplot2::element_line(color = "black"), 
-      axis.title = ggplot2::element_text(color = grey(.10, 1)), 
-      axis.text =  ggplot2::element_text(color = grey(.20, 1)), 
+      axis.title = ggplot2::element_text(color = col_txt_1), 
+      axis.text =  ggplot2::element_text(color = col_txt_2), 
+      axis.ticks = ggplot2::element_line(color = col_txt_2), 
       # legend: 
-      legend.title = ggplot2::element_text(color = grey(.10, 1)), 
-      legend.text = ggplot2::element_text(color = grey(.20, 1)), 
+      legend.title = ggplot2::element_text(color = col_txt_1), 
+      legend.text = ggplot2::element_text(color = col_txt_2), 
       legend.background = ggplot2::element_blank(), 
       legend.key = ggplot2::element_blank(), 
       # strip: 
-      # strip.background = ggplot2::element_rect(fill = pal_seeblau[[1]], color = pal_seeblau[[5]], size = ggplot2::rel(5/3)), 
-      strip.background = ggplot2::element_rect(fill = grey(.95, 1), color = grey(.10, 1), size = ggplot2::rel(5/3)), 
-      strip.text = ggplot2::element_text(color = grey(.20, 1)), 
+      # strip.background = ggplot2::element_blank(), 
+      strip.background = ggplot2::element_rect(fill = grey(.95, 1), color = grey(.10, 1),  # light grey strip background  
+                                               size = ggplot2::rel(1.0)), 
+      strip.text = ggplot2::element_text(color = col_txt_3, size = ggplot2::rel(1.0), 
+                                         margin = ggplot2::margin(t = 5, r = 5, b = 5, l = 5, unit = "pt")), 
       # panel: 
       # panel.border = ggplot2::element_blank(), 
-      panel.border = ggplot2::element_rect(fill = "transparent", color = grey(.10, 1), linetype = "solid", size = ggplot2::rel(2/3)), 
+      panel.border = ggplot2::element_rect(fill = "transparent", color = grey(.10, 1), linetype = "solid", 
+                                           size = ggplot2::rel(2/3)), 
       # panel.grid = ggplot2::element_blank(), 
-      panel.grid.major = ggplot2::element_line(color = grey(.80, 1), linetype = "solid", size = ggplot2::rel(1/3)), 
-      panel.grid.minor = ggplot2::element_line(color = grey(.80, 1), linetype = "dotted", size = ggplot2::rel(1/3)), 
-      panel.background = ggplot2::element_blank(), 
+      panel.grid.major = ggplot2::element_line(color = grey(.80, 1), linetype = "solid",  size = ggplot2::rel(1/3)), 
+      panel.grid.minor = ggplot2::element_line(color = grey(.80, 1), linetype = "dotted", size = ggplot2::rel(1/4)), 
+      # panel.background = ggplot2::element_blank(),  # no panel background 
+      panel.background = ggplot2::element_rect(fill = grey(1.0, 1), color = NA), # AE: "white" panel background 
       # background:  
-      plot.background = ggplot2::element_rect(fill = "transparent", color = NA), 
+      plot.background = ggplot2::element_rect(fill = col_bg, color = NA), 
       complete = TRUE)
   
 } # theme_ds4psy end. 
-
-### Check: 
-# 
-# # (1) Iris data: 
-# 
-# ggplot(datasets::iris) +
-#   geom_jitter(aes(x = Petal.Length, y = Petal.Width, color = Species), size = 3, alpha = 2/3) +
-#   labs(title = "Iris species",
-#        caption = "Data from datasets::iris") +
-#   theme_ds4psy(col_title = "black", base_size = 11)
-# 
-# # (2) ggplot2::diamonds:
-# 
-# ggplot(diamonds, aes(x = x, y = price, color = cut)) +
-#   geom_jitter(alpha = 4/6) +
-#   # geom_violin(color = Seeblau, fill = NA) +
-#   # facet_wrap(~cut) +
-#   scale_color_manual(values = unikn::usecol(pal = viridisLite::magma(n = 5))) +
-#   labs(tag = "A",
-#        title = "Plot title",
-#        # subtitle = "Subtitle of this plot",
-#        x = "Label of x-axis", y = "Labe of y-axis",
-#        color = "Color label:",
-#        caption = "Data from ggplot2::diamonds") +
-#   theme_ds4psy(col_title = unikn::pal_seeblau[[5]], base_size = 11)
-# 
-# # (3) ggplot2::mpg:
-# 
-# ggplot(mpg, aes(x = cty, y = hwy, color = manufacturer)) +
-#   geom_jitter(size = 2, alpha = 2/3) +
-#   facet_wrap(~cyl) +
-#   scale_color_manual(values = unikn::usecol(pal = c(rev(pal_seeblau), "white", pal_bordeaux), n = 15)) +
-#   labs(tag = "B",
-#        title = "Car data plot",
-#        # subtitle = "Demo of some faceted plot",
-#        caption = "Data from ggplot2::mpg") +
-#   theme_ds4psy(col_title = "darkblue", base_size = 11)
 
 ## ToDo: ----------
 
